56691ed2dc9ac4ae32bdfff091207198f8520870
Pete Bachant <petebachant@gmail.com>
2014-01-21 11:46:04 -0500

    Fix indentation

--- acspy/control.py
+++ acspy/control.py
@@ -14,15 +14,15 @@ class Control(object):
             self.hc = acsc.openCommDirect()
         elif self.contype == "Ethernet":
             self.hc = acsc.openCommEthernetTCP(address="10.0.0.100", port=701)
-			
+	
     def enable_axis(self, axis, wait=acsc.SYNCHRONOUS):
         acsc.enable(self.hc, axis, wait)
-		
+
     def disable_axis(self, axis, wait=acsc.SYNCRHONOUS):
         acsc.disable(self.hc, axis, wait)
             
     def ptp(self, axis, target, flags=None, wait=acsc.SYNCHRONOUS):
         acsc.toPoint(self.hc, flags, axis, target, wait)
-		
+
 	def disconnect(self):
-        acsc.closeComm(self.hc)
+        acsc.closeComm(self.hc)        
