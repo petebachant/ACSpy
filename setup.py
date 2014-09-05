bbd22937aeaef04cd194c22c8c4c4246569e7fbf
Pete Bachant <petebachant@gmail.com>
2014-09-05 10:06:39 -0400

    Put on PyPI

--- setup.py
+++ setup.py
@@ -11,7 +11,20 @@ setup(
     packages=['acspy'],
     scripts=[],
     url='https://github.com/petebachant/ACSpy.git',
-    license='LICENSE',
+    license='GPL v3',
     description='Package for working with ACS motion controllers.',
-    long_description=open('README.md').read()
+    long_description=open('README.md').read(),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4"]
 )
