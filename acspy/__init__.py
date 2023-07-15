4d94617562310d165b42ae0bcf58cee53d674819
Pete Bachant <pete@windesco.com>
2023-07-15 09:37:25 -0700

    Clean up __init__.py

--- acspy/__init__.py
+++ acspy/__init__.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-ACSpy
------
-
-A package for working with ACS motion controllers in Python.
-"""
+"""A package for working with ACS motion controllers in Python."""
 
 __version__ = "0.0.4"
